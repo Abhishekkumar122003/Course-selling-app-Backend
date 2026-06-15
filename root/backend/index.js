a832c795b1d2cec0ad01bd9c3108c8b3119f64be
Abhishekkumar122003 <abhishek801503gupta@gmail.com>
2026-06-15 19:18:04 +0530

    trying to increase my github contrybution strick 😁

--- root/backend/index.js
+++ root/backend/index.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const json = require("jonwebtoken");
 const app = express();
-
-const {userModel , adminModel , courseModel , purcheseModel} = require("./Model")
+const {}
+const {userModel , adminModel , courseModel , purcheseModel} = require("./Model");
+const { createCourseRoutes } = require('./courses');
+const { createUserRoutes } = require('./users');
 
 const SECRET = "sdnvvj3v";
 app.use(express.json());
 
 app.get("/" , (req, res)=>{});
-
+createUserRoutes(app);
+createCourseRoutes(app)
 
 app.listen(3000);
