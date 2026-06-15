8cef6452588f2e7677c3f8012212b24c7dbc77f0
Abhishekkumar122003 <abhishek801503gupta@gmail.com>
2026-06-15 16:49:32 +0530

    now the signin part is completed.

--- root/backend/index.js
+++ root/backend/index.js
@@ -20,18 +20,42 @@ app.post("/signup" , async(req, res)=>{
     })
     if(!userExists){
         const newuser = await userModel.create({
+            email:email,
             firstName:firstName,
             lastName:lastName,
             password:userPassword
         })
     }else{
         res.status(411).json({
-            message:"user with this creadential already exist "
+            message:"user with this creadential already exist. "
         })
     }
 
 })
-app.post("/signin" , (req, res)=>{})
+app.post("/signin" , async (req, res)=>{
+    const email= req.body.email
+    const password = req.body.Password;
+    
+    const userExists = await userModel.findOne({
+        email:email,
+        password:password
+    })
+    if(!userExists){
+        res.status(409).json({
+            message:"Either the given Cradential is wrong or the User does not SIGNUP"
+        })
+    }else{
+        const token = json.sign({
+            id: userExists._id
+        } , SECRET);
+
+        res.json({
+            token:token
+        })
+    }
+
+
+})
 
 app.post("/purchase" , (req, res)=>{});
 
