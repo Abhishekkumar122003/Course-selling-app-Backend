410ec8a2751a521557e0b9536d79739c75bb1611
Abhishekkumar122003 <abhishek801503gupta@gmail.com>
2026-06-17 15:40:15 +0530

    added the admin signup route, nexti am going to implement the signin routes. stay consistent buddy your future self will proude of you.

--- root/backend/routes/admin.js
+++ root/backend/routes/admin.js
@@ -7,15 +7,35 @@ const { adminModel , courseModel } = require("../Model")
 
 
 
-adminRouter.post("/signup" , (req, res)=>{
+adminRouter.post("/signup" ,async  (req, res)=>{
     const { firstName , lastName, password , email  } = req.body;
-    console.log(firstName +"\n"+ lastName , email , password)
-    res.json({
+    
+    const userExist = await adminModel.findOne({
+        email:email,
         firstName:firstName,
-        lastName:lastName,
-        password:password,
-        email:email
+        lastName:lastName
     })
+    if(userExist){
+        res.json({
+            message:"Admin with the given creadential already exist"
+        })
+    }else{
+        try{
+            await adminModel.create({
+                email:email,
+                firstName:firstName,
+                lastName:lastName,
+                password:password
+            })
+            res.send({
+                message: "The ADMIN with given Creadential CREATED. "
+            })
+        }catch(err){
+            console.log("Some error happend during Admin signup" +"\n" + err)
+        }
+    }
+
+
 })
 module.exports={
     adminRouter:adminRouter
